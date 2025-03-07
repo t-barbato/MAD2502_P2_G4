fd8c85db31a44cf999802c5752cafbdf871dbff4
elliana :) <elliana.kirby@ufl.edu>
2025-03-07 16:46:28 -0500

    get_complex_grid

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def get_escape_time(c: complex, max_iterations: int) -> int | None:
     z = c
     if abs(z) > 2:
@@ -7,3 +9,12 @@ def get_escape_time(c: complex, max_iterations: int) -> int | None:
         if abs(z) > 2:
             return i + 1
     return None
+
+def get_complex_grid(top_left: complex, bottom_right: complex, step: float) -> np.ndarray:
+    # num_rows = len(np.arange(bottom_right.imag, top_left.imag, step))
+    # num_cols = len(np.arange(top_left.real, bottom_right.real, step))
+    reals = np.arange(top_left.real, bottom_right.real, step)
+    imags = np.arange(top_left.imag, bottom_right.imag, -1 * step)
+    # imags = np.flip(imags)
+    imags = (imags.reshape((len(imags), 1)))
+    return reals + (imags * 1j)
