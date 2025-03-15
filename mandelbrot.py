180a0058be20750881b0b0d9b4665bbcb52d6a3a
elliana :) <elliana.kirby@ufl.edu>
2025-03-15 15:31:15 -0400

    Deleted comments. Final push :)

--- mandelbrot.py
+++ mandelbrot.py
@@ -23,29 +23,6 @@ def get_escape_time(c: complex, max_iterations: int) -> int | None:
             return i + 1
     return None
 
-# def get_escape_time_alt(c: np.array(complex), max_iterations: int) -> int:
-#     """
-#     Takes a complex number and gets the escape time for it. The escape time is the moment when the absolute value of the
-#     magnitude is greater than 2.
-#
-#     Parameters:
-#         c: complex number
-#         max_iterations: int
-#
-#     Return:
-#         number of iterations it takes to escape within range of max_iterations. If it does not escape,
-#         returns max_iterations + 1
-#
-#         """
-#     z = c
-#     if np.abs(z) > 2:
-#         return 0
-#     for i in range(max_iterations):
-#         z = z**2 + c
-#         if np.abs(z) > 2:
-#             return i + 1
-#     return max_iterations + 1
-
 
 def get_complex_grid(top_left: complex, bottom_right: complex, step: float) -> np.ndarray:
     """
@@ -64,25 +41,6 @@ def get_complex_grid(top_left: complex, bottom_right: complex, step: float) -> n
     imags = np.arange(top_left.imag, bottom_right.imag, -1 * step)
     imags = (imags.reshape((len(imags), 1)))
     return reals + (imags * 1j)
-
-#nested loop version
-# def get_escape_time_color_arr(c_arr: np.ndarray, max_iterations: int) -> np.ndarray:
-#     """
-#     Assigns a color value to each item in the array of complex numbers for the Mandelbrot Set.
-#
-#     Parameters:
-#         c_arr: array of complex numbers
-#         max_iterations: int
-#
-#     Returns:
-#         np.ndarray of color values
-#     """
-#     color_arr = np.zeros_like(c_arr).real
-#     for i in range(len(c_arr)):
-#         for j in range(len(c_arr)):
-#             color_arr[i][j] = get_escape_time(c_arr[i][j], max_iterations)
-#     color_arr = (max_iterations-color_arr+1)/(max_iterations+1)
-#     return color_arr
 
 
 def get_escape_time_color_arr(c_arr: np.ndarray,max_iterations: int):
@@ -135,45 +93,3 @@ def get_julia_color_arr(c_arr: np.ndarray, c: complex, max_iterations: int) -> n
     color_arr = (max_iterations - escape_times + 1) / (max_iterations + 1)
 
     return color_arr
-
-# nested loop format
-# def get_julia_color_arr(c_arr: np.ndarray, c: complex, max_iterations: int) -> np.ndarray:
-#     """
-#         Assigns a color value to each item in the array of complex numbers of the Julia Set.
-#
-#         Parameters:
-#             c_arr: array of complex numbers
-#             max_iterations: int
-#
-#         Returns:
-#             np.ndarray of color values
-#         """
-#     color_arr = np.zeros_like(c_arr).real
-#     for i in range(len(c_arr)):
-#         for j in range(len(c_arr)):
-#             color_arr[i][j] = get_escape_time_julia(c_arr[i][j], c, max_iterations)
-#     color_arr = (max_iterations-color_arr+1)/(max_iterations+1)
-#     return color_arr
-
-# def get_escape_time_julia(start: np.ndarray, c: complex, max_iterations: int) -> int:
-#     """
-#     Takes a complex number grid and gets the escape time for it. The escape time is the moment when the absolute value
-#     of the magnitude is greater than 2.
-#
-#     Parameters:
-#         start: complex number grid
-#         c: complex number
-#         max_iterations: int
-#
-#     Return:
-#         int of number of iterations it takes to escape within range of max_iterations. If it does not escape,
-#         returns max_iterations + 1:
-#     """
-#     z = start
-#     if np.abs(z) > 2:
-#         return 0
-#     for i in range(max_iterations):
-#         z = z ** 2 + c
-#         if np.abs(z) > 2:
-#             return i + 1
-#     return None
